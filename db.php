6c2c17c0e1143b14400013204d010e4a185f287d
Jules Remille <98144009+Roulio-sauvage@users.noreply.github.com>
2023-09-02 23:40:13 +0200

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "eu-cdbr-west-03.cleardb.net"; // L'adresse du serveur MySQL (généralement "localhost" en local)
 $username = "be5f7d4ff522ca"; // Le nom d'utilisateur par défaut pour XAMPP est souvent "root"
-$password = "cd46a061"; // Laissez le mot de passe vide par défaut
+$password = "7d0327f554127f2"; // Laissez le mot de passe vide par défaut
 $database = "heroku_878af3535d076e4"; // Remplacez par le nom de votre base de données
 
 // Créer une connexion
