16d6e072d059fff0fab11239f35cbdc0a594f862
Jules Remille <98144009+Roulio-sauvage@users.noreply.github.com>
2023-09-11 07:12:39 +0200

    Update classes.php

--- classes.php
+++ classes.php
@@ -35,54 +35,8 @@ $numeros = range(1, 13); // Crée un tableau avec les numéros de 1 à 12
 </head>
 <body>
     <div class="container mt-5">
-        <h3 class="text-center">Le site fermera ses portes le dimanche 10 septembre 2023</h3>
-        <br>
-        <h1 class="text-center">Classes à JDA : année 2023</h1>
-        <br>
-        <br>
-        <div class="text-center">
-            <h3>Aller à :</h3>
-            <a href="#seconde" class="btn btn-primary" style='scroll-behavior: smooth;'>Seconde</a>
-            <a href="#premiere" class="btn btn-primary" style='scroll-behavior: smooth;'>Première</a>
-            <a href="#terminale" class="btn btn-primary" style='scroll-behavior: smooth;'>Terminale</a>
-            <br>
-            <br>
-        </div>
-        <?php
-        // Boucle sur les classes
-        foreach ($classes as $classe) {
-            if($classe=='2'){echo "<strong><h2 id='seconde'>Classe : Seconde</h2></strong>";}
-            echo "<br>";
-            if($classe=='1'){echo "<strong><h2 id='premiere'>Classe : Première</h2></strong>";}
-            echo "<br>";
-            if($classe=='T'){echo "<strong><h2 id='terminale'>Classe : Terminale</h2></strong>";}
-            foreach ($numeros as $numero) {
-                $sql = "SELECT nom, prenom FROM jda WHERE classe = '$classe-$numero'";
-                $result = mysqli_query($conn, $sql);
-                if($classe=='2'){echo "<h5 >2de $numero</h5>";}
-                if($classe=='1'){echo "<h5 >1er $numero</h5>";}
-                if($classe=='T'){echo "<h5 >Tle $numero</h5>";}
-                echo "<table class='table table-bordered custom-table'>";
-                echo "<thead><tr><th>Nom</th><th>Prénom</th></tr></thead>";
-                echo "<tbody>";
-                
-                while ($row = mysqli_fetch_assoc($result)) {
-                    echo "<tr>";
-                    echo "<td>" . $row['nom'] . "</td>";
-                    echo "<td>" . $row['prenom'] . "</td>";
-                    echo "</tr>";
-                }
-
-                echo "</tbody>";
-                echo "</table>";
-            }
-        }
-        ?>
+        <h3 class="text-center">Ce site est fermé.</h3>
     </div>
-
-    <footer class="text-center mt-5">
-        <p><em>Créé par Trix et Roulio. En cas de problème, contacter : lebgjules50@gmail.com</em></p>
-    </footer>
     
     <!-- Ajout du script JavaScript de Bootstrap (facultatif) -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
